3bdb7189c63b2ab80b7d66d76ab061c46095639e
unknown <somanagendra.s@gmail.com>
2022-07-27 18:22:22 -0700

    latest

---
+++ SampleTest/src/main/java/com/nag/test/SampleTestApplication.java
@@ -0,0 +1,21 @@
+package com.nag.test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@SpringBootApplication
+public class SampleTestApplication {
+
+	 @GetMapping("/hai")
+	  public static String hai() {
+	        return "Hello World!";
+	    }
+
+	public static void main(String[] args) {
+		SpringApplication.run(SampleTestApplication.class, args);
+	}
+
+}
---
+++ SampleTest/src/test/java/com/nag/test/SampleTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nag.test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SampleTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
